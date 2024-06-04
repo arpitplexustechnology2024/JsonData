4d7a536d5947e034ed8fee3d8627d9d547edb5f0
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-04 15:27:04 -0700

    Commit

---
+++ JsonData/Controller/HandyJsonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HandyJsonViewController.swift
+//  JsonData
+//
+//  Created by Arpit iOS Dev. on 04/06/24.
+//
+
+import UIKit
+
+class HandyJsonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JsonData/Model/DataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  DataLoader.swift
+//  JsonData
+//
+//  Created by Arpit iOS Dev. on 04/06/24.
+//
+
+import Foundation
